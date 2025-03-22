178c45a6b4a11756df1f4b508da5988e5ac4b562
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-22 11:12:56 -0400

    fiz testes my data test

--- src/Components/MyAccountComponents/MyPerfilComponents/MyDataComponents/MyData/MyData.test.tsx
+++ src/Components/MyAccountComponents/MyPerfilComponents/MyDataComponents/MyData/MyData.test.tsx
@@ -21,4 +21,56 @@ describe('MyData', () => {
     const header = screen.getByRole('heading', { name: /Meu perfil/i });
     expect(header).toBeInTheDocument();
   });
+
+  test('should render header-2', () => {
+    render(
+      <MemoryRouter>
+        <MyData />
+      </MemoryRouter>
+    );
+
+    const header = screen.getByRole('heading', {
+      name: /Li, compreendi e concordo com as Condições Gerais.*inclusive com relação à proteção de dados pessoais, finalidades e hipóteses de tratamento de dados.: Sim/i,
+    });
+    expect(header).toBeInTheDocument();
+  });
+
+  test('should render button change password', () => {
+    render(
+      <MemoryRouter>
+        <MyData />
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button', {
+      name: /ALTERAR SENHA/i,
+    });
+    expect(button).toBeInTheDocument();
+  });
+
+  test('should render button update your email', () => {
+    render(
+      <MemoryRouter>
+        <MyData />
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button', {
+      name: /ATUALIZAR SEU E-MAIL/i,
+    });
+    expect(button).toBeInTheDocument();
+  });
+
+  test('should render button change data', () => {
+    render(
+      <MemoryRouter>
+        <MyData />
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button', {
+      name: /ALTERAR DADOS/i,
+    });
+    expect(button).toBeInTheDocument();
+  });
 });
